83636f05b7963b53ea82d50b9ea5190abf188d94
lucas-campanga <lucas.campagna@hp.com>
2024-07-28 15:11:54 -0300

    refactoring sheets

--- src/hooks/sheets.js
+++ src/hooks/sheets.js
@@ -1,86 +1,169 @@
-const useSheets = (apiKey, userId)=>{
-    if(apiKey || userId){
-        if(apiKey) localStorage.setItem('apiKey', apiKey)
-        if(userId) localStorage.setItem('userId', userId)
-        const sheets = useSheets()
-        if(!sheets) return null
-        return sheets.isValidUser()
-            .then(({message, status})=>status && message ? sheets : null)
-            .catch(e=>{console.log('error on isValidUser');return null})
-    }else{
-        apiKey = localStorage.getItem('apiKey');
-        userId = localStorage.getItem('userId');
+function useSheets(apiKey, userId) {
+    if (apiKey && userId) {
+        logout();
+        storage.set('apiKey', apiKey)
+        const userIds = storage.get('userIds') || [userId];
+        if (!userIds.includes(userId)) {
+            userIds.push(userId)
+        }    
+        storage.set('userIds', userIds)
+        storage.set('userId', userId)
+        return useSheets()
     }
-    if(!apiKey || !userId)
-        return null
+    apiKey = storage.get('apiKey');
+    userId = storage.get('userId');
+    if (!apiKey || !userId) {
+        return
+    }
     const url = getURL(apiKey)
-    const buildGet = name => (params={}) => fetchCached(buildUrl(url, {userId, action: {name, params}}))
-    const buildPost = name => (params={}) => fetchCached(url, {method: 'POST', mode:'cors', body: JSON.stringify({userId, action: {name, params}})}).then(r=>{invalidateCache();return r})
+    const buildGet = (key, {action, actions}) =>
+        fetchCached(key, url, {userId, action, actions})
+    const buildPost = async (key, {action, actions}) =>{
+        invalidate(key);
+        const response = await fetchCached(key, url, {userId, action, actions})
+        invalidate(key);
+        return response;
+    }
+    const buildTable = function (name, hasGet, hasAdd, hasRm) {
+        let ret = {}
+        ret.invalidate = () => invalidate(name)
+        const buildObject = content => {
+            const obj = {};
+            obj[name] = content;
+            return obj;
+        }
+        if (hasGet) {
+            ret.get = () => buildGet(name, {action: buildObject({get: {}})})
+        }
+        if (hasAdd) {
+            ret.add = props => buildPost(name, {action: buildObject({add: props})})
+            ret.adds = listOfProps => buildPost(name, {actions: listOfProps.map( props => buildObject({add: props}))})
+        }
+        if (hasRm) {
+            ret.rm = props => buildPost(name, {action: buildObject({rm: props})})
+            ret.rms = listOfProps => buildPost(name, {actions: listOfProps.map( props => buildObject({rm: props}))})
+        }
+        ret.error = () => storage.error.get(name)
+        return ret
+    }
+    const fetchAllKeys = ['attendance', 'student', 'teacher']
     return {
-        ...Object.fromEntries([
-            ...getRoutes.map(n => [n, buildGet(n)]),
-            ...postRoutes.map(n => [n, buildPost(n)]),
-        ]),
-        apiKey,
-        userId,
-        invalidateCache,
+        apiKey: () => storage.get('apiKey'),
+        userId: () => storage.get('userId'),
+        attendance: buildTable('attendance', true, true, true),
+        requisition: buildTable('requisition', false, true, false),
+        student: buildTable('student', true, false, false),
+        teacher: buildTable('teacher', true, false, false),
+        date: buildTable('date', true, false, false),
+        fetchAll: () => fetchCachedMany(fetchAllKeys, url, {userId, actions: fetchAllKeys.map(k => {const obj = {}; obj[k] = {get: {}}; return obj})}), 
+        isValidUser: async function(){
+            const obj = await this.teacher.get()
+            return obj && ['name', 'className', 'theme'].reduce((o,n) => o && (n in obj), true)
+        },
+        addUserId: userId => {
+            const userIds = storage.get('userIds') || []
+            if (!userIds.includes(userId)) {
+                userIds.push(userId)
+                storage.set('userIds', userIds)
+            }
+        },
+        getUserIds: () => storage.get('userIds'),
+        logout,
     }
 }
 
 const getURL = apiKey=>`https://script.google.com/macros/s/${apiKey}/exec`
-
-const getRoutes = [
-    'isValidUser',
-    'getDates',
-    'getMyStudents',
-    'getMyPresencesData',
-    'getHistory',
-    'checkIsValidDate',
-    'getUserInfo',
-]
-
-const postRoutes = [
-    'setHistory',
-]
-
-function buildUrl(url, params){
-    return `${url}?${(new URLSearchParams(joinObject(params)).toString())}`
+const invalidate = key => {
+    storage.cache.rm(key);
+    storage.error.rm(key);
+}
+const logout = () => {
+    const apiKey = storage.get('apiKey');
+    const userId = storage.get('userId');
+    if (apiKey && userId) {
+        storage.clear();
+    }
 }
 
-function joinObject(obj, parentKey = '') {
-    let result = {};
-    for (let key in obj) {
-        let currentKey = parentKey ? `${parentKey}.${key}` : key;
-        if (typeof obj[key] === 'object' && obj[key] !== null) {
-            let nested = joinObject(obj[key], currentKey);
-            result = { ...result, ...nested };
-        } else {
-            result[currentKey] = obj[key];
-        }
+const fetchCachedMany = async (keys, url, body)=>{
+    if (!keys) {
+        throw "No keys provided to fetchCached"
     }
-    return result;
-}
-
-// var cache = {}
-// const invalidateCache = ()=>cache={}
-const invalidateCache = ()=>localStorage.setItem('cache','{}')
-// invalidateCache()
-
-const fetchCached = async (url, args)=>{
-    const key = url + JSON.stringify(args)
-    let cache = JSON.parse(localStorage.getItem('cache') || '{}')
-    if(key in cache){
-        return cache[key]
-    }
+    keys.forEach(invalidate);
     try{
-        const response = await fetch(url, args).then(p=>p.json())
-        cache[key] = Object.assign({}, response)
-        localStorage.setItem('cache',JSON.stringify(cache))
-        return response;
-    } catch(err){
-        localStorage.clear()
-        return false
+        const response = await fetch(url, {method: 'POST', mode:'cors', body: JSON.stringify(body)})
+            .then(p=>p.json())
+            .catch(e => e.message)
+        response.forEach((resp,i) => {
+            (resp.success ? storage.cache.set : storage.error.set)(keys[i], resp.message)
+            return resp.message;
+        });
+    } catch(error) {
+        console.log('[fetchCachedMany] Error:', error)
+        return false;
     }
 }
+const fetchCached = async (key, url, body)=>{
+    if (!key) {
+        throw "No key provided to fetchCached"
+    }
+    const previousResult = storage.cache.get(key)
+    if (previousResult) {
+        return previousResult;
+    }
+    invalidate(key);
+    try{
+        const response = await fetch(url, {method: 'POST', mode:'cors', body: JSON.stringify(body)})
+            .then(p=>p.json())
+            .catch(e => e.message)
+            .then(r => {if(r.success) return r.message; throw r.message})
+        storage.cache.set(key, response);
+        return response;
+    } catch(error) {
+        storage.error.set(key, error);
+        return false;
+    }
+}
+
+function Storage(moduleName){
+    this.moduleName = moduleName;
+    const storageType = localStorage;
+    const iterateThrough = callback => Object.fromEntries(Object.keys(storageType).filter(k => k.startsWith(moduleName)).map(k => [k, callback(k)]))
+    const _getKey = key => !key ? moduleName : (key.startsWith(moduleName) ? key : `${moduleName}.${key}`);
+    this.get = key => {if(!key) return iterateThrough((k,i) => this.get(k)); const v = storageType.getItem(_getKey(key)); try{return JSON.parse(v)} catch {return v}};
+    this.set = (key, item) => storageType.setItem(_getKey(key), JSON.stringify(item));
+    this.rm = key => storageType.removeItem(_getKey(key));
+    this.clear = key => {
+        key = _getKey(key);
+        iterateThrough(k => k.startsWith(key) ? storageType.removeItem(k) : null)
+    }
+    return this;
+}
+
+const storage = new Storage('sheets')
+storage.cache = new Storage('sheets.cache')
+storage.error = new Storage('sheets.error')
 
 export default useSheets;
+
+if (process.env.NODE_ENV === 'test') {
+    const apiKey = process.env.REACT_APP_APIKEY;
+    const userId = process.env.REACT_APP_USERID;
+    const assert = console.assert;
+    const sheet = useSheets(apiKey, userId);
+    assert(sheet.apiKey() == apiKey);
+    assert(sheet.userId() == userId);
+    assert(await sheet.isValidUser());
+    assert(sheet.getUserIds().includes(userId));
+    const students = await sheet.student.get();
+    assert(students)
+    assert(students.length > 0);
+    const mockStudentAttendance = students[0].name;
+    assert(await sheet.attendance.add({name: mockStudentAttendance}));
+    const attendances = await sheet.attendance.get();
+    const lastAttendance = attendances.at(-1);
+    assert(lastAttendance.name == mockStudentAttendance);
+    assert(await sheet.attendance.rm({name: mockStudentAttendance}));
+    assert(await sheet.requisition.add({description: 'unit teste description'}))
+    assert(await sheet.date.get())
+}
